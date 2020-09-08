ee8159f80b0b9fed80f31a4b78a8fac1a9505cbd
Noelia Parra <65130588+Noelia-Parra@users.noreply.github.com>
2020-09-08 21:45:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector(".form-control");
+  let span = document.querySelector("span");
+  if (searchInput.value) {
+    span.innerHTML = `${searchInput.value}`;
+    searchCity(searchInput.value);
+  } else {
+    span.innerHTML = null;
+    alert("Please type a city");
+  }
+}
+function searchCity(city) {
+  let apiKey = "efdb90508378d538c76c3382a5e5f2bb";
+  let urlKey = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&&units=metric`;
+  axios.get(`${urlKey}&appid=${apiKey}`).then(showTemperature);
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = `${temperature}`;
+}
+searchCity("Madrid");
 
